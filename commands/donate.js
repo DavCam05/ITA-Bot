042e6f2adc8edd0487fe3f94696af60a0f07bc2d
DavCam05 <davidecammarano2005@gmail.com>
2020-09-15 11:06:49 +0100

    fixed spelling mistake

--- commands/donate.js
+++ commands/donate.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "Sends the link to patreon. (soon to be changed to StreamElements Tip page)",
     execute(message, args) {
         message.channel.send("Here are places where you can support the owner of the bot:");
-        message.channel.send("htts://www.patreon.com/davidecammarano");
+        message.channel.send("https://www.patreon.com/davidecammarano");
         message.channel.send("https://streamelements.com/davcam0055/tip");
     }
 }
